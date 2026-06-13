8262eea583d421d9b24295647b7d27b15b1e910d
RAIHAN KABIR <124366115+razvanbibi@users.noreply.github.com>
2026-06-13 08:09:22 +0600

    Clarify ABI comment in contract.ts
    Updated comment to clarify the purpose of the ABI.

--- src/lib/contract.ts
+++ src/lib/contract.ts
@@ -9,7 +9,7 @@ export const CELODAILY_VAULT_CONTRACT =
   "0x6ea4C7e400cC455712e284883E74B49402C5C818" as const;
 export const CELO_CHAIN_ID_HEX = "0xa4ec"; // 42220
 
-// দরকারি ফাংশনের  ABI
+// ফাংশনের  ABI
 export const OXTXN_STREAK_ABI = [
   // reads
   {
