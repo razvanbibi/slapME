428b007b71c51f0decf6da7e94cee2807647475c
RAIHAN KABIR <124366115+razvanbibi@users.noreply.github.com>
2026-06-13 23:40:36 +0600

    Remove empty line before AvatarBubbleStream function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,7 +28,6 @@ type Supporter = {
 const USDM_TOKEN_ADDRESS = "0x765DE816845861e75A25fCA122bb6898B8B1282a";
 
 
-
 function AvatarBubbleStream({ avatar }: { avatar: string }) {
   const [bubbles, setBubbles] = useState<
     { id: number; left: number; size: number }[]
