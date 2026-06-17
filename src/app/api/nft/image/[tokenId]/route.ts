e4cc5abde3831c732ab9c3dc10a4ea705be3dfb5
RAIHAN KABIR <124366115+razvanbibi@users.noreply.github.com>
2026-06-18 01:05:07 +0600

    Fix SVG formatting in route.ts

--- src/app/api/nft/image/[tokenId]/route.ts
+++ src/app/api/nft/image/[tokenId]/route.ts
@@ -38,7 +38,6 @@ const logoBase64 = await fetchAsBase64(
 <svg width="600" height="360" viewBox="0 0 600 360"
   xmlns="http://www.w3.org/2000/svg"
   xmlns:xlink="http://www.w3.org/1999/xlink">
-
   <defs>
     <!-- bg -->
     <linearGradient id="bg" x1="0" y1="0" x2="1" y2="1">
