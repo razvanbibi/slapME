1b8dadc3ef380396ac018a2b39acb27ee2d2bccf
RAIHAN KABIR <124366115+razvanbibi@users.noreply.github.com>
2026-06-20 02:34:26 +0600

    Fix formatting issue in TodayMessageLoop.tsx

--- src/app/TodayMessageLoop.tsx
+++ src/app/TodayMessageLoop.tsx
@@ -44,7 +44,7 @@ export default function TodayMessageLoop({ isDarkMode, account }: Props) {
     setTimeout(() => {
       setIndex((i) => (i + 1) % slides.length);
       setVisible(true);
-    }, 300); // fade
+    }, 300);
   }, current.duration);
   return () => clearTimeout(timer);
 }, [index, slides]);
