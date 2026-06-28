b3602392534b4a23110ca5f59db9940b5a03b23d
razvanbibi <mstrazvanbibi@gmail.com>
2026-06-28 15:48:31 +0600

    Four new export const added in Contract.ts

---
+++ src/lib/contract.ts
@@ -0,0 +1,7 @@
+export const APP_NAME = "SlapMe";
+
+export const NETWORK = "mainnet";
+
+// এখানে পরে তোমার Mainnet Contract Address বসাবো
+export const CONTRACT_ADDRESS = "";
+export const CONTRACT_NAME = "slap-me";
