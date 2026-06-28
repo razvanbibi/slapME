8054dea01294b000beb4f434f8efef3039641ac1
razvanbibi <mstrazvanbibi@gmail.com>
2026-06-28 23:50:35 +0600

    Transactions.ts updated with contract logic

--- src/lib/transactions.ts
+++ src/lib/transactions.ts
@@ -1,40 +1,63 @@
 "use client";
 
-import {
-  openContractCall,
-} from "@stacks/connect";
+import { openContractCall } from "@stacks/connect";
 
 import {
   AnchorMode,
   PostConditionMode,
 } from "@stacks/transactions";
 
+import { STACKS_MAINNET } from "@stacks/network";
+
 import {
   CONTRACT_ADDRESS,
   CONTRACT_NAME,
 } from "./contract";
 
+const network = STACKS_MAINNET;
+
 export async function slap() {
-  await openContractCall({
+  return openContractCall({
+    network,
+
     contractAddress: CONTRACT_ADDRESS,
     contractName: CONTRACT_NAME,
 
     functionName: "slap",
-
     functionArgs: [],
 
-    network: "mainnet",
-
-    anchorMode: AnchorMode.Any,
-
+    anchorMode: AnchorMode.OnChainOnly,
     postConditionMode: PostConditionMode.Allow,
 
-    onFinish(data) {
-      console.log("TX Success", data);
+    onFinish: (data) => {
+      console.log("Slap TX Success:", data);
     },
 
-    onCancel() {
-      console.log("Cancelled");
+    onCancel: () => {
+      console.log("Slap cancelled");
+    },
+  });
+}
+
+export async function punch() {
+  return openContractCall({
+    network,
+
+    contractAddress: CONTRACT_ADDRESS,
+    contractName: CONTRACT_NAME,
+
+    functionName: "punch",
+    functionArgs: [],
+
+    anchorMode: AnchorMode.OnChainOnly,
+    postConditionMode: PostConditionMode.Allow,
+
+    onFinish: (data) => {
+      console.log("Punch TX Success:", data);
+    },
+
+    onCancel: () => {
+      console.log("Punch cancelled");
     },
   });
 }
