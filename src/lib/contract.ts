45996994e3c61740fdf0c053a224bb60e1e9c234
razvanbibi <mstrazvanbibi@gmail.com>
2026-06-28 18:29:10 +0600

    Mainnet deployed contract address added as export const

--- src/lib/contract.ts
+++ src/lib/contract.ts
@@ -2,6 +2,7 @@ export const APP_NAME = "SlapMe";
 
 export const NETWORK = "mainnet";
 
-// এখানে পরে তোমার Mainnet Contract Address বসাবো
-export const CONTRACT_ADDRESS = "";
+export const CONTRACT_ADDRESS =
+  "SPTR1GDYG6VGAQ8JGHSGHN8FPBSNWAGVY59MKM4J";
+
 export const CONTRACT_NAME = "slap-me";
