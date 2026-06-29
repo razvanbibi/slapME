c8cb832b22bc2a8c647e01707ad35dfbc20fa028
razvanbibi <mstrazvanbibi@gmail.com>
2026-06-29 14:42:30 +0600

    export async function getGlobalStats() implemented

--- src/lib/read.ts
+++ src/lib/read.ts
@@ -1,15 +1,32 @@
 import {
-  fetchCallReadOnlyFunction,
-  cvToJSON,
-  principalCV,
-  uintCV,
+    fetchCallReadOnlyFunction,
+    cvToJSON,
+    principalCV,
+    uintCV,
 } from "@stacks/transactions";
 
 import { STACKS_MAINNET } from "@stacks/network";
 
 import {
-  CONTRACT_ADDRESS,
-  CONTRACT_NAME,
+    CONTRACT_ADDRESS,
+    CONTRACT_NAME,
 } from "./contract";
 
 const network = STACKS_MAINNET;
+
+export async function getGlobalStats() {
+    const result = await fetchCallReadOnlyFunction({
+        network,
+
+        contractAddress: CONTRACT_ADDRESS,
+        contractName: CONTRACT_NAME,
+
+        functionName: "get-global-stats",
+
+        functionArgs: [],
+
+        senderAddress: CONTRACT_ADDRESS,
+    });
+
+    return cvToJSON(result);
+}
