8cdde1c0decad8b7386852bb5fb6f6c2cd5a029a
razvanbibi <mstrazvanbibi@gmail.com>
2026-06-29 14:43:32 +0600

    export async function getUserStats(address: string) added in read.ts

--- src/lib/read.ts
+++ src/lib/read.ts
@@ -30,3 +30,22 @@ export async function getGlobalStats() {
 
     return cvToJSON(result);
 }
+
+export async function getUserStats(address: string) {
+    const result = await fetchCallReadOnlyFunction({
+        network,
+
+        contractAddress: CONTRACT_ADDRESS,
+        contractName: CONTRACT_NAME,
+
+        functionName: "get-user-stats",
+
+        functionArgs: [
+            principalCV(address),
+        ],
+
+        senderAddress: address,
+    });
+
+    return cvToJSON(result);
+}
